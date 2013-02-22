562907409c76cd7052b18510dc5926198919d240
Maximilien Riehl <maximilien.riehl@gmail.com>
2013-02-22 09:47:48 +0100

    the module name pythonbuilder is deprecated

--- build.py
+++ build.py
@@ -13,7 +13,7 @@
 #
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from pythonbuilder.core import use_plugin, init, Author
+from pybuilder.core import use_plugin, init, Author
 
 use_plugin('python.core')
 use_plugin('python.install_dependencies')
