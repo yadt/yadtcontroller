40a3e9fc4c3b153adb4e60dd820c0a8294fac3e6
Maximilien Riehl <maximilien.riehl@gmail.com>
2013-02-25 15:47:22 +0100

    copying setup.cfg to dist

--- build.py
+++ build.py
@@ -19,6 +19,7 @@ use_plugin('python.core')
 use_plugin('python.install_dependencies')
 
 use_plugin('filter_resources')
+use_plugin('copy_resources')
 
 use_plugin('python.coverage')
 use_plugin('python.distutils')
@@ -46,6 +47,8 @@ def set_properties(project):
     project.depends_on('yadtbroadcast-client')
 
     project.get_property('filter_resources_glob').append('**/yadtcontroller/__init__.py')
+    project.get_property('copy_resources_glob').append('setup.cfg')
+
 
 @init(environments="teamcity")
 def set_properties_for_teamcity(project):
