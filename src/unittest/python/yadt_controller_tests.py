ef0885cbd5e06542dff116c6cd024d7e5cae2b79
mriehl <maximilien.riehl@gmail.com>
2013-03-18 14:20:44 +0100

    looks like mockito on PyPy is very sensitive to the order of kwargs

--- src/unittest/python/yadt_controller_tests.py
+++ src/unittest/python/yadt_controller_tests.py
@@ -105,10 +105,10 @@ class YadtControllerTests(unittest.TestCase):
 
         verify(self.event_handler_mock).initialize_for_execution_request(waiting_timeout=2, pending_timeout=3,
                                                                          command_to_execute='foo',
-                                                                         tracking_id='test',
                                                                          arguments=['bar',
                                                                                     'baz',
-                                                                                    '--tracking-id=test'])
+                                                                                    '--tracking-id=test'],
+                                                                         tracking_id='test')
 
     def test_should_not_initialize_for_info_when_info_option_was_not_given(self):
         when(yadt_controller).docopt(any_value(), version=any_value()).thenReturn({'--config-file': '/configuration',
